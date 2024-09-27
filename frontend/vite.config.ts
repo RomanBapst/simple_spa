ad2186f14a9cbbad66eb4acc52c2745e9f48aa23
Roman Bapst <bapstroman@gmail.com>
2024-09-27 07:22:44 +0300

    vite-config: use env variable for root path
    Signed-off-by: Roman Bapst <bapstroman@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/webclass/',
+  base: process.env.VITE_ROOT,
 })
