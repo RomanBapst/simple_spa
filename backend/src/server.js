4002e0c6783f1e1f6c767f95f159727819756056
Roman Bapst <bapstroman@gmail.com>
2025-03-28 08:16:08 +0300

    test printing up of user
    Signed-off-by: Roman Bapst <bapstroman@gmail.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,6 +28,9 @@ app.use(express.json());
 
 app.post('/', async (req, res) => {
     try {
+        console.log("getting a request!!!!!!!!!!!!!!!!!!!!!!!")
+        const userIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+        console.log("user ip " + userIp)
         const { name, surname, instagram, inTouchOption, watchTimeOption, timestamp, device } = req.body;
         
         // Make the request to Google Apps Script API
